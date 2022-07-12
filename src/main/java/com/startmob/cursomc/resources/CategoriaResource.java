1f8c27c64b8feb4d921c6dcb14916b03382e1363
Leonardo Maier <107595862+leleoreidelas@users.noreply.github.com>
2022-07-12 13:43:05 -0300

    paginacao com parametros opcionais na requisicao

--- src/main/java/com/startmob/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/startmob/cursomc/resources/CategoriaResource.java
@@ -6,11 +6,13 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -25,20 +27,21 @@ public class CategoriaResource {
 	private static final String List = null;
 	@Autowired
 	private CategoriaService service;
+	private Integer page2;
 
 	@RequestMapping(value = "{id}", method = RequestMethod.GET)
 	public ResponseEntity<Optional<Categoria>> find(@PathVariable Integer id) {
 		Optional<Categoria> obj = service.find(id);
-		
+
 		System.out.println(obj);
-		
+
 		System.out.println(id);
-		
+
 		return ResponseEntity.ok().body(obj);
 
 	}
 
-	@RequestMapping(method=RequestMethod.POST)
+	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Categoria> insert(@RequestBody Categoria obj) {
 		obj = service.insert(obj);
 		System.out.println("insert");
@@ -48,32 +51,42 @@ public class CategoriaResource {
 
 	}
 
-	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id ) {
-		
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+
 		System.out.println("");
-				obj = service.update(obj);
-				System.out.println("update");
-				System.out.println(obj);
-				System.out.println(id);
+		obj = service.update(obj);
+		System.out.println("update");
+		System.out.println(obj);
+		System.out.println(id);
 		return ResponseEntity.noContent().build();
 	}
-	
 
 	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
-	@RequestMapping( method = RequestMethod.GET)
-	public ResponseEntity< List<CategoriaDTO>> findAll() {
-		 List<Categoria> list = service.findAll();
-		 List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 
 	}
 
-	}
-	
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "page", defaultValue = "0") Integer Page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
 
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
+		return ResponseEntity.ok().body(listDto);
+
+	}
+
+}
